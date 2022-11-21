8631b32c95a2d479681f8634aa28f05e47c6ea93
Saief Zneti <saief_zaneti@yahoo.com>
2022-11-21 09:13:30 +0100

    Added deposit & withdraw

--- transactions-app/transactions/src/main/java/com/plutus/transactions/dtos/responses/TransactionResponse.java
+++ transactions-app/transactions/src/main/java/com/plutus/transactions/dtos/responses/TransactionResponse.java
@@ -1,8 +1,14 @@
 package com.plutus.transactions.dtos.responses;
 
-public class TransactionResponse {
-    private int transactionId;
-    private int accountId;
+public class TransactionResponse extends AppResponse {
+
+    private long transactionId;
     private String timestamp ;
     private double amount;
+
+    public TransactionResponse(long transactionId, String timestamp, double amount) {
+        this.transactionId = transactionId;
+        this.timestamp = timestamp;
+        this.amount = amount;
+    }
 }
